6d9936c3de267040bb5a041bbe19da4d9f2c21fe
Andy Zou <andyzou1302@gmail.com>
2024-02-09 18:46:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from starlette.responses import FileResponse 
 from fastapi.staticfiles import StaticFiles
-from fastapi import Request
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
-app.mount("/", StaticFiles(directory="static", html = True), name="static" )
+@app.get("/")
+async def root():
+    return FileResponse("static/index.html")
