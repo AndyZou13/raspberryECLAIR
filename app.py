ec0506032247368f31ea0179a3781eeadc4d5d9d
Andy Zou <andyzou1302@gmail.com>
2024-02-15 00:14:24 -0500

    update

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
-from flask import Flask, render_template, url_for, request
+from flask import Flask, render_template, url_for, redirect, flash
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '163c1af8b4a801e8fddec7e72b6db4dd'
 
-username = "Andy"
+global username
 
 @app.route('/')
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def loginPage():
     form = LoginForm()
-    return render_template('login.html', form = form)
+    title = "Login"
+    if form.validate_on_submit():
+        global username 
+        username = form.username.data
+        flash(f'Logged in as {form.username.data}', 'Success')
+        return redirect(url_for('profilePage'))
+    return render_template('login.html', form = form, title=title)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def registerPage():
     form = RegistrationForm()
-    return render_template('register.html', form = form)
+    title = "Register"
+    if form.validate_on_submit():
+        global username 
+        username = form.username.data
+        flash(f'Account created for {form.username.data}', 'Success')
+        return redirect(url_for('profilePage'))
+    return render_template('register.html', form = form, title=title)
 
 @app.route('/profile')
 def profilePage():
--- forms.py
+++ forms.py
@@ -1,12 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, HiddenField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=5, max=20)])
     password = StringField('Password', validators=[DataRequired()])
     confirmPassword = StringField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    
     submit = SubmitField('Register')
     
 class LoginForm(FlaskForm):
