efecfec78c50d274d55890c5b47986406096d1fa
Andy Zou <andyzou1302@gmail.com>
2024-03-14 01:00:06 -0400

    update

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, HiddenField
+from wtforms import StringField, SubmitField, BooleanField, DateTimeLocalField
 from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
@@ -13,3 +13,9 @@ class LoginForm(FlaskForm):
     password = StringField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+class BookingForm(FlaskForm):
+    datum = DateTimeLocalField('Datum', validators=[DataRequired()])
+    submit3 = SubmitField('Tier 3 \n 48 amps max output \n 11.5kW of power per hour')
+    submit2 = SubmitField('Tier 2 \n 40 amps max output \n 9.6kW of power per hour')
+    submit1 = SubmitField('Tier 1 \n 32 amps max output \n 7.7kW of power per hour')
