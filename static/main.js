65534ef5bc855ded3d40e9d313a2f932f65ef05a
Andy Zou <andyzou1302@gmail.com>
2024-04-11 11:01:34 -0400

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,11 +1,11 @@
 console.log("Sanity check!");
 
-fetch("https://raspberryeclair.azurewebsites.net/config")
+fetch("/config")
 .then((result) => { return result.json(); })
 .then((data) => {
   const stripe = Stripe(data.publicKey);
   document.querySelector("#bookingForm").addEventListener("submit", () => {
-    fetch("https://raspberryeclair.azurewebsites.net/create-checkout-session")
+    fetch("/create-checkout-session")
     .then((result) => { return result.json(); })
     .then((data) => {
       console.log(data);
